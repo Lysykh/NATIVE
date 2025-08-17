a0ae8494ecf2361b2fb5a8a164868173273b47e7
Lysykh <68994584+Lysykh@users.noreply.github.com>
2025-08-17 21:52:15 +0300

    Немного откатился 250817

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import SportSelect from './SelectSport';
 import SelectSportLevel from './SelectSportLevel';
